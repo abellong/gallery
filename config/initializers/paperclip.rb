013ab5814692b778341ba3a91f2f98795bae04d4
Yongbiao Long <longyongbiao@gmail.com>
2014-12-12 10:37:15 +0800

    bug fix

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,7 +3,7 @@ Paperclip::Attachment.default_options[:qiniu_credentials] = {
   :access_key => ENV['QINIU_ACCESS_KEY'] || raise("set env QINIU_ACCESS_KEY"),
   :secret_key => ENV['QINIU_SECRET_KEY'] || raise("set env QINIU_SECRET_KEY")
 }
-Paperclip::Attachment.default_options[:bucket] = 'photos-longyb-info'
+Paperclip::Attachment.default_options[:bucket] = "#{ENV['BUCKET_NAME']}"
 Paperclip::Attachment.default_options[:use_timestamp] = false
 Paperclip::Attachment.default_options[:qiniu_host] =
   "http://#{ENV['BUCKET_NAME']}.qiniudn.com"
